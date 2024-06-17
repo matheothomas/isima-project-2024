9b37e159e33841235cbcc83580a547cdb731afa3
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-17 15:47:06 +0200

    Initialised main.c

--- matheo/animation/src/main.c
+++ matheo/animation/src/main.c
@@ -3,11 +3,16 @@
  * date : 17-06-24
  */
 
+#include "SDL2/SDL_render.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
 
 
 int main(void){
+
 
 	return 0;
 }
