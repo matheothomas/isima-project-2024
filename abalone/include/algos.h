3fd0c70c387d45bf2e6035eb5a6f788b8cf063b9
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-18 13:30:10 +0200

    Created tree structure

--- abalone/include/algos.h
+++ abalone/include/algos.h
@@ -3,11 +3,24 @@
  * date : 18-06-24
  */
 
+#include "init.h"
 #ifndef algos_h 
 #define algos_h
 
 /* Struct definitions */
 
+typedef struct play {
+	cell_t *c;
+	int direction;
+} play_t;
+
+typedef struct tree {
+	struct tree *tree_tab;
+	play_t play;
+	int tab_length;
+	int value;
+	int depth;
+} tree_t;
 
 
 /* Functions definitions */
