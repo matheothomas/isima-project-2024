6a397a4b75ace469001d9df8978f7f6b4eb82cdf
domi <domi@localhost.localdomain>
2024-06-18 19:35:38 +0200

    working version of the function create_clean_board

--- abalone/src/init.c
+++ abalone/src/init.c
@@ -6,9 +6,10 @@
 #include "../include/init.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 cell_t *create_cell() {
-  cell_t *c;
+  cell_t *c = malloc(sizeof(cell_t));
   c->state = EMPTY;
 
   for (int i = 0; i < 6; i++) {
@@ -19,6 +20,7 @@ cell_t *create_cell() {
 }
 
 board_t create_clean_board() {
+
   board_t b;
   cell_t *first_cell = create_cell();
   b.cell = first_cell;
@@ -40,25 +42,52 @@ board_t create_clean_board() {
   // creation of other cells
 
   for (int i = 0; i < 6; i++) {
+    c = 0;
+    j = 0;
     cur_cell = b.cell->neighbourg[i];
 
     while (c < 3) {
+
       for (int n = 0; n < 6; n++) {
         if (n != 3 && ((c != 0) || (c == 0 && n != 4))) {
-          if (c == 0 && n == 2) {
-            cur_cell->neighbourg[i + n] =
+          if (n == 2) {
+            cur_cell->neighbourg[(i + n) % 6] =
                 cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 1) % 6];
-            cur_cell->neighbourg[i + n]->neighbourg[(i + n + 3) % 6] = cur_cell;
-          } else if (n == 4) {
-            cur_cell->neighbourg[i + n] =
+            cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                cur_cell;
+          } else if (n == 4 && i == 0) {
+            cur_cell->neighbourg[(i + n) % 6] =
                 cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 5) % 6];
-            cur_cell->neighbourg[i + n]->neighbourg[(i + n + 3) % 6] = cur_cell;
+            cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                cur_cell;
           } else {
-            cur_cell->neighbourg[i + n] = create_cell();
-            cur_cell->neighbourg[i + n]->neighbourg[(i + n + 3) % 6] = cur_cell;
+            if (n == 5 && i != 0) {
+              cur_cell->neighbourg[(i + n) % 6] =
+                  cur_cell->neighbourg[(i + n + 5) % 6]->neighbourg[i];
+              cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                  cur_cell;
+            } else if (n == 4 && i != 0) {
+              cur_cell->neighbourg[(i + n) % 6] =
+                  cur_cell->neighbourg[(i + n + 5) % 6]
+                      ->neighbourg[(i + 5) % 6];
+              cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                  cur_cell;
+            } else {
+              if (i == 5 && n == 1 && c == 0) {
+                cur_cell->neighbourg[(i + n) % 6] =
+                    b.cell->neighbourg[0]->neighbourg[5];
+                cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                    cur_cell;
+              } else {
+                cur_cell->neighbourg[(i + n) % 6] = create_cell();
+                cur_cell->neighbourg[(i + n) % 6]->neighbourg[(i + n + 3) % 6] =
+                    cur_cell;
+              }
+            }
           }
         }
       }
+
       cur_cell = cur_cell->neighbourg[i];
       c += 1;
     }
@@ -69,58 +98,84 @@ board_t create_clean_board() {
     cur_cell->neighbourg[i] = NULL;
     cur_cell->neighbourg[(i + 1) % 6] = NULL;
     cur_cell->neighbourg[(i + 5) % 6] = NULL;
-    for (int n = 2; n < 5; n += 2) {
-      cur_cell->neighbourg[i] = create_cell();
-      cur_cell->neighbourg[i]->neighbourg[(i + 3) % 6] = cur_cell;
-    }
+
+    cur_cell->neighbourg[(i + 2) % 6] =
+        cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 1) % 6];
+    cur_cell->neighbourg[(i + 2) % 6]->neighbourg[(i + 2 + 3) % 6] = cur_cell;
+
+    cur_cell->neighbourg[(i + 4) % 6] =
+        cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 5) % 6];
+    cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 4 + 3) % 6] = cur_cell;
 
     while (c < 2) {
-      cur_cell = cur_cell->neighbourg[i + j];
+      cur_cell = cur_cell->neighbourg[(i + j) % 6];
       cur_cell->neighbourg[i] = NULL;
       cur_cell->neighbourg[(i + 1) % 6] = NULL;
-      cur_cell->neighbourg[i + 2] = create_cell();
-      cur_cell->neighbourg[i + 2]->neighbourg[(i + 2 + 3) % 6] = cur_cell;
+      if (c == 1 && i == 5) {
+        cur_cell->neighbourg[(i + 2) % 6] =
+            b.cell->neighbourg[0]->neighbourg[0]->neighbourg[0]->neighbourg[5];
+        cur_cell->neighbourg[(i + 2) % 6]->neighbourg[(i + 2 + 3) % 6] =
+            cur_cell;
 
-      if (c == 0) {
-        cur_cell->neighbourg[i + 3] =
-            cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 2) % 6];
-        cur_cell->neighbourg[i + 3]->neighbourg[(i + 3 + 3) % 6] = cur_cell;
-      } else {
-        cur_cell->neighbourg[i + 3] = create_cell();
-        cur_cell->neighbourg[i + 3]->neighbourg[(i + 3 + 3) % 6] = cur_cell;
-        cur_cell->neighbourg[i + 4] =
+        cur_cell->neighbourg[(i + 3) % 6] =
+            b.cell->neighbourg[0]->neighbourg[0]->neighbourg[5];
+        cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 3 + 3) % 6] =
+            cur_cell;
+
+        cur_cell->neighbourg[(i + 4) % 6] =
             cur_cell->neighbourg[(i + 5) % 6]->neighbourg[(i + 3) % 6];
-        cur_cell->neighbourg[i + 4]->neighbourg[(i + 4 + 3) % 6] = cur_cell;
+        cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 4 + 3) % 6] =
+            cur_cell;
+
+      } else {
+        cur_cell->neighbourg[(i + 2) % 6] = create_cell();
+        cur_cell->neighbourg[(i + 2) % 6]->neighbourg[(i + 2 + 3) % 6] =
+            cur_cell;
+
+        if (c == 0) {
+          cur_cell->neighbourg[(i + 3) % 6] =
+              cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 2) % 6];
+          cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 3 + 3) % 6] =
+              cur_cell;
+        } else {
+          cur_cell->neighbourg[(i + 3) % 6] = create_cell();
+          cur_cell->neighbourg[(i + 3) % 6]->neighbourg[(i + 3 + 3) % 6] =
+              cur_cell;
+          cur_cell->neighbourg[(i + 4) % 6] =
+              cur_cell->neighbourg[(i + 5) % 6]->neighbourg[(i + 3) % 6];
+          cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 4 + 3) % 6] =
+              cur_cell;
+        }
       }
+
       c += 1;
     }
 
     c = 0;
     j = 3;
 
-    cur_cell = cur_cell->neighbourg[i + j];
+    cur_cell = cur_cell->neighbourg[(i + j) % 6];
 
-    cur_cell->neighbourg[i + 1] =
-        cur_cell->neighbourg[(i + 0) % 6]->neighbourg[(i + 2) % 6];
-    cur_cell->neighbourg[i + 1]->neighbourg[(i + 1 + 3) % 6] = cur_cell;
+    cur_cell->neighbourg[(i + 1) % 6] =
+        cur_cell->neighbourg[i]->neighbourg[(i + 2) % 6];
+    cur_cell->neighbourg[(i + 1) % 6]->neighbourg[(i + 1 + 3) % 6] = cur_cell;
 
-    cur_cell->neighbourg[i + 5] =
-        cur_cell->neighbourg[(i + 0) % 6]->neighbourg[(i + 4) % 6];
-    cur_cell->neighbourg[i + 5]->neighbourg[(i + 5 + 3) % 6] = cur_cell;
+    cur_cell->neighbourg[(i + 5) % 6] =
+        cur_cell->neighbourg[i]->neighbourg[(i + 4) % 6];
+    cur_cell->neighbourg[(i + 5) % 6]->neighbourg[(i + 5 + 3) % 6] = cur_cell;
 
-    cur_cell->neighbourg[i + 4] = cur_cell->neighbourg[(i + 5) % 6]
-                                      ->neighbourg[(i + 4) % 6]
-                                      ->neighbourg[(i + 2) % 6];
-    cur_cell->neighbourg[i + 4]->neighbourg[(i + 4 + 3) % 6] = cur_cell;
+    cur_cell->neighbourg[(i + 4) % 6] = cur_cell->neighbourg[(i + 5) % 6]
+                                            ->neighbourg[(i + 4) % 6]
+                                            ->neighbourg[(i + 2) % 6];
+    cur_cell->neighbourg[(i + 4) % 6]->neighbourg[(i + 4 + 3) % 6] = cur_cell;
 
     j = 4;
 
-    cur_cell = cur_cell->neighbourg[i + j];
+    cur_cell = cur_cell->neighbourg[(i + j) % 6];
 
-    cur_cell->neighbourg[i + 0] =
+    cur_cell->neighbourg[i] =
         cur_cell->neighbourg[(i + 1) % 6]->neighbourg[(i + 5) % 6];
-    cur_cell->neighbourg[i + 0]->neighbourg[(i + 0 + 3) % 6] = cur_cell;
+    cur_cell->neighbourg[i]->neighbourg[(i + 3) % 6] = cur_cell;
   }
-
   return b;
 }
