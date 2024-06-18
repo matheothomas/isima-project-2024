7ad120cca838d21520c1620dc6d8d322b873bbae
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-18 11:45:15 +0200

    Created board structure

--- abalone/include/init.h
+++ abalone/include/init.h
@@ -7,11 +7,29 @@
 #define init_h
 
 /* Struct definitions */
+typedef enum state {
+	EMPTY,
+	WHITE,
+	BLACK,
+} state_e;
+
+typedef struct cell {
+	state_e state;
+	struct cell *neighbourg[6];
+} cell_t;
+
+typedef struct board {
+	cell_t *cell;			// pointer to the first cell of the board
+	int n_white;
+	int n_black;
+} board_t;
 
 
 
 /* Functions definitions */
 
+/// creates a cell of the board
+cell_t create_cell();
 
 #endif
 
