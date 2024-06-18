d1035d4edbdd87547c2f99de1df9c17a95ea2048
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-18 10:23:32 +0200

    Added sdl base code to print board

---
+++ abalone/include/graphics.h
@@ -0,0 +1,24 @@
+/* name : graphics.h
+ * author : matheo thomas
+ * date : 18-06-24
+ */
+
+#include <SDL2/SDL.h>
+
+#ifndef graphics_h 
+#define graphics_h
+
+
+
+/* Functions definitions */
+
+void end_sdl(char ok, char const *msg, SDL_Window *window, SDL_Renderer *renderer);
+
+SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *window, SDL_Renderer *renderer );
+
+void texturing(SDL_Texture* my_texture, SDL_Window* window, SDL_Renderer* renderer);
+
+
+
+#endif
+
---
+++ abalone/src/graphics.c
@@ -0,0 +1,73 @@
+/* name : graphics.c
+ * author : matheo thomas
+ * date : 18-06-24
+ */
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+void end_sdl(char ok,                               // normal end : ok = 0 ; abnormal ok = 1
+			 char const *msg,                       // message to print
+			 SDL_Window *window,                    // window to close
+			 SDL_Renderer *renderer) {              // renderer to close
+	char msg_formated[255];                                            
+	int l;                                                     
+
+	if (!ok) {										// printing what is wrong
+		strncpy(msg_formated, msg, 250);                                         
+		l = strlen(msg_formated);                                            
+		strcpy(msg_formated + l, " : %s\n");                                     
+
+		SDL_Log(msg_formated, SDL_GetError());                                   
+	}                                                          
+
+	if (renderer != NULL) {							// renderer's destruction if necessary
+		SDL_DestroyRenderer(renderer);
+		renderer = NULL;
+	}
+	if (window != NULL)   {                         // windows destruction if necessary
+		SDL_DestroyWindow(window);
+		window= NULL;
+	}
+
+	SDL_Quit();                                                    
+
+	if (!ok) {
+		exit(EXIT_FAILURE);                                                  
+	}                                                          
+}
+
+SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *window, SDL_Renderer *renderer ){
+	SDL_Surface *my_image = NULL;
+	SDL_Texture* my_texture = NULL;
+
+	my_image = IMG_Load(file_image_name);
+	if (my_image == NULL) end_sdl(0, "Chargement de l'image impossible", window, renderer);
+
+	my_texture = SDL_CreateTextureFromSurface(renderer, my_image);
+	SDL_FreeSurface(my_image);
+	if (my_texture == NULL) end_sdl(0, "Echec de la transformation de la surface en texture", window, renderer);
+
+	return my_texture;
+}
+
+void texturing(SDL_Texture* my_texture, SDL_Window* window, SDL_Renderer* renderer) {
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_Rect 
+	source = {0},
+	window_dimensions = {0},
+	destination = {0};
+
+	SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h);
+	SDL_QueryTexture(my_texture, NULL, NULL, &source.w, &source.h);
+
+	float zoom = 0.4;
+	destination.w = source.w * zoom;         // the destination is a source's zoom
+	destination.h = source.h * zoom;
+	destination.x = (window_dimensions.w - destination.w) / 2;
+	destination.y = (window_dimensions.h - destination.h) / 2;
+
+
+	// SDL_RenderCopyEx(renderer, my_texture, &source, &destination, j*0.1, &point, SDL_FLIP_NONE);
+	SDL_RenderCopy(renderer, my_texture, &source, &destination);
+}
