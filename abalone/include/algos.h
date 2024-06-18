14844abf914b1186b06675c2e35fdb659b88ef14
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-18 14:50:07 +0200

    Created tree and play structures

--- abalone/include/algos.h
+++ abalone/include/algos.h
@@ -3,20 +3,23 @@
  * date : 18-06-24
  */
 
+#include <stdbool.h>
 #include "init.h"
+
 #ifndef algos_h 
 #define algos_h
 
 /* Struct definitions */
 
 typedef struct play {
-	cell_t *c;
+	cell_t *cell_tab[5];
+	int cell_tab_length;
 	int direction;
 } play_t;
 
 typedef struct tree {
 	struct tree *tree_tab;
-	play_t play;
+	play_t *play;
 	int tab_length;
 	int value;
 	int depth;
@@ -24,6 +27,18 @@ typedef struct tree {
 
 
 /* Functions definitions */
+
+/// returns the best play depending on the player
+play_t *choose_play(tree_t *tree, bool is_bot);
+
+/// returns a tree containing all available plays for a board
+tree_t *gen_plays(tree_t *tree, board_t *board);
+
+/// applies a play to the board
+tree_t *apply_play(tree_t *tree, board_t *board, play_t *play, bool is_bot);
+
+/// applies the min-max algorithm 
+tree_t *eval(tree_t *tree, board_t *board, int max_depth, bool is_bot);
 
 
 #endif
