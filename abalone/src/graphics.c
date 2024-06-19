ef54b8104859defb135014d49a36e4e42afe7f43
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-19 10:29:19 +0200

    Added first version of cell display function

--- abalone/src/graphics.c
+++ abalone/src/graphics.c
@@ -34,7 +34,7 @@ void end_sdl(char ok,                               // normal end : ok = 0 ; abn
 
 	if (!ok) {
 		exit(EXIT_FAILURE);                                                  
-	}                                                          
+	}            
 }
 
 SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *window, SDL_Renderer *renderer ){
@@ -53,10 +53,7 @@ SDL_Texture* load_texture_from_image(char  *  file_image_name, SDL_Window *windo
 
 void texturing(SDL_Texture* texture, SDL_Window* window, SDL_Renderer* renderer) {
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_Rect 
-	source = {0},
-	window_dimensions = {0},
-	destination = {0};
+	SDL_Rect source = {0}, window_dimensions = {0}, destination = {0};
 
 	SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h);
 	SDL_QueryTexture(texture, NULL, NULL, &source.w, &source.h);
@@ -72,6 +69,52 @@ void texturing(SDL_Texture* texture, SDL_Window* window, SDL_Renderer* renderer)
 	SDL_RenderCopy(renderer, texture, &source, &destination);
 }
 
-void display_cell(SDL_Texture *texture, SDL_Window *window, SDL_Renderer *renderer) {
+void display_cell(SDL_Texture *texture, SDL_Window *window, SDL_Renderer *renderer, int id) {
+	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_Rect source = {0}, window_dimensions = {0}, destination = {0};
+	int i, j;
 
+	SDL_GetWindowSize(window, &window_dimensions.w, &window_dimensions.h);
+	SDL_QueryTexture(texture, NULL, NULL, &source.w, &source.h);
+
+	float zoom = 9;
+	destination.w = window_dimensions.w / zoom;
+	destination.h = window_dimensions.h / zoom;
+	
+	if(id < 5) {
+		i = 1;
+		j = id;
+	} else if (id < 11) {
+		i = 2;
+		j = id - 5;
+	} else if (id < 18) {
+		i = 3;
+		j = id - 11;
+	} else if (id < 26) {
+		i = 4;
+		j = id - 18;
+	} else if (id < 35) {
+		i = 5;
+		j = id - 26;
+	} else if (id < 43) {
+		i = 6;
+		j = id - 35;
+	} else if (id < 50) {
+		i = 7;
+		j = id - 43;
+	} else if (id < 56) {
+		i = 8;
+		j = id - 50;
+	} else if (id < 61) {
+		i = 9;
+		j = id - 56;
+	}
+
+	destination.x = destination.w * i;
+	destination.y = destination.h * j;
+
+
+	// SDL_RenderCopyEx(renderer, my_texture, &source, &destination, j*0.1, &point, SDL_FLIP_NONE);
+	SDL_RenderCopy(renderer, texture, &source, &destination);
+	
 }
