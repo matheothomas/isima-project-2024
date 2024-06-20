b1b4f0335f8eb2dc3094c15d53b31edad2312608
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-20 23:48:00 +0200

    Documented functions

--- abalone/include/utilities.h
+++ abalone/include/utilities.h
@@ -6,25 +6,44 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#include "init.h"
 #include "algos.h"
+#include "init.h"
 
-/* Struct definitions */
 
 /* Functions definitions */
+
+/// prints the content of a play for debugging purposes
 void print_play(play_t *play);
+
+/// tests if two plays are identical
 bool is_duplicate(play_t * play1, play_t * play2);
+
+/// tests if a play is valid
 bool validity_play(play_t * play, bool player);
-void free_tree(tree_t * tree);
+
+/// creats a tree
 tree_t * create_tree(play_t * play, int value, int depth);
+
+/// frees a tree
+void free_tree(tree_t * tree);
+
+/// adds a node to the linked list tree
 void append_tree(tree_t * tree, play_t * play, int value, int depth);
-void fill_play_buffer(play_t * play);
-void cell_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
-void cell_does_not_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
-void traversal_rec(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
-tree_t * gen_plays(board_t * board, int depth, bool player);
+
+/// fills the buffer of a play
 void fill_play_buffer(play_t * play);
 
-/// creates a cell of the board
+/// part of the traversal_rec function, case where the following cell of a play belongs to the player
+void cell_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
+
+/// part of the traversal_rec function, case where the following cell of a play does not belong to the player
+void cell_does_not_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
+
+/// recursively generates plays
+void traversal_rec(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
+
+/// generates all valid plays available given a board and a player
+tree_t * gen_plays(board_t * board, int depth, bool player);
+
 
 #endif
