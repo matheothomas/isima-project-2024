a8173522a0c4da7248672fa643d97276ac037db2
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-20 08:23:35 +0200

    Put sdl structures initialisations in a function

--- abalone/src/main.c
+++ abalone/src/main.c
@@ -20,41 +20,19 @@
 #include "utilities.h"
 
 int main(void) {
-	SDL_Window *window = NULL;
-	SDL_Renderer *renderer = NULL;
-	SDL_DisplayMode screen;
-	SDL_Texture *board, *white, *black;
+	graphics_t *g = init_sdl();
 
-	// SDL INITIALISATION
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
-		end_sdl(0, "ERROR SDL INIT", window, renderer);
+	// TTF_Font * font=TTF_OpenFont("res/Unique.ttf", 72 );
 
-	SDL_GetCurrentDisplayMode(0, &screen);
-
-	// Window creation
-	window = SDL_CreateWindow("Premier dessin", SDL_WINDOWPOS_CENTERED,
-						   SDL_WINDOWPOS_CENTERED, screen.h * 0.9 * 1.5,
-						   screen.h * 0.9, SDL_WINDOW_OPENGL);
-
-	if (window == NULL)
-		end_sdl(0, "ERROR WINDOW CREATION", window, renderer);
-
-	// Renderer creation
-	renderer = SDL_CreateRenderer(
-		window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (renderer == NULL)
-		end_sdl(0, "ERROR RENDERER CREATION", window, renderer);
-
-	// Textures creation
-	board = load_texture_from_image("res/board.png", window, renderer);
-	white = load_texture_from_image("res/white.png", window, renderer);
-	black = load_texture_from_image("res/black.png", window, renderer);
-
-	TTF_Font * font=TTF_OpenFont("res/Unique.ttf", 72 );
-
-	board_t * b = create_clean_board();
+	board_t *b = create_clean_board();
+	b = start_config(b);
 	cell_t **cell_tab=create_table(*b);
 
+	/////////////////////////////
+	// SDL MAIN LOOP FUNCTIONS //
+	/////////////////////////////
+
+	/*
 	// First Event Loop
 	int h;
 	int w;
@@ -64,7 +42,7 @@ int main(void) {
 	int r2=0;
 	int r=0;
 	int mouse_state=0;
-	SDL_GetWindowSize(window, &w, &h);
+	SDL_GetWindowSize(g->window, &w, &h);
 
 	// Rect creation
 	SDL_Rect* text_box = crea_rect(w/3, h/4, w/3, h/4);
@@ -75,9 +53,9 @@ int main(void) {
 	SDL_Rect* confirm = crea_rect(7*w/9, 4*h/11, w/9, h/11);
 	SDL_Rect* button = crea_rect(13*w/18, 6*h/11, 2*w/9, 2*w/9);
 	
-	SDL_Texture*text=create_texture_for_text("choose your\nstarting line !", font, window, renderer);
-	texturing(text,window,renderer);
-	SDL_RenderPresent(renderer);
+	SDL_Texture*text=create_texture_for_text("choose your\nstarting line !", font, g->window, g->renderer);
+	texturing(text,g->window, g->renderer);
+	SDL_RenderPresent(g->renderer);
 	SDL_Delay(200);
 
 	SDL_bool program_on = SDL_TRUE;
@@ -137,15 +115,15 @@ int main(void) {
 		}
 		
 		// render
-		home_menu(window, renderer, text_box, button_1, button_2, font, text, r1, r2);
+		// home_menu(g->window, g->renderer, text_box, button_1, button_2, font, text, r1, r2);
 		SDL_Delay(1);
 	}
 	
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	SDL_RenderClear(renderer);
+	SDL_SetRenderDrawColor(g->renderer, 255, 255, 255, 255);
+	SDL_RenderClear(g->renderer);
 			
 	// Second Event Loop
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+	SDL_SetRenderDrawColor(g->renderer, 255, 255, 255, 255);
 	while (program_on_2) {
 		
 		if (SDL_PollEvent(&event)) {
@@ -179,16 +157,17 @@ int main(void) {
 			if(x>7*w/9 && 7*w/9+w/9){
 				if(4*h/11 && 4*h/11+h/11){
 					program_on_2 = SDL_FALSE;
-					SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-					SDL_RenderClear(renderer);
+					SDL_SetRenderDrawColor(g->renderer, 255, 255, 255, 255);
+					SDL_RenderClear(g->renderer);
 				}
 			}
 			else{
-				display_game(window, renderer, text_box_2, confirm, button, font, text, r, board, white, black, cell_tab);
+				// display_game(g->window, g->renderer, text_box_2, confirm, button, font, text, r, board, g->white, g->black, cell_tab);
 				SDL_Delay(1);
 			}
 		}
 	}
+	*/
 
 
 	// TEST FUNCTIONS
@@ -219,7 +198,7 @@ int main(void) {
 
 	// algos.c tests
 	
-	display_board(board, white, black, window, renderer, cell_tab);
+	display_board(g->board, g->white, g->black, g->window, g->renderer, cell_tab);
 	SDL_Delay(1000);
 
 	play_t *p = malloc(sizeof(play_t));
@@ -264,26 +243,26 @@ int main(void) {
 	}
 	*/
 
-	cell_tab[16]->state = BLACK;
-	display_board(board, white, black, window, renderer, cell_tab);
-	SDL_Delay(2000);
+	// cell_tab[16]->state = BLACK;
+	// display_board(g->board, g->white, g->black, g->window, g->renderer, cell_tab);
+	// SDL_Delay(2000);
 
 	p = choose_play(b);
 
 	b = apply_play(b, p);
 
-	display_board(board, white, black, window, renderer, cell_tab);
+	display_board(g->board, g->white, g->black, g->window, g->renderer, cell_tab);
 	SDL_Delay(4000);
 
 
 
 
 
-	SDL_RenderClear(renderer);
+	SDL_RenderClear(g->renderer);
 
 	IMG_Quit();
 
-	end_sdl(1, "Normal ending", window, renderer);
+	end_sdl(1, "Normal ending", g->window, g->renderer);
 
 	return 0;
 
