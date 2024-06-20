2574da7487835691d6c0cc9cadb40a6ff8e406c7
Elloim <eloi.petit.pro@protonmail.com>
2024-06-20 16:35:30 +0200

    Added missing fill_play_buffer function prototype

--- abalone/include/utilities.h
+++ abalone/include/utilities.h
@@ -18,6 +18,7 @@ bool validity_play(play_t * play, bool player);
 void free_tree(tree_t * tree);
 tree_t * create_tree(play_t * play, int value, int depth);
 void append_tree(tree_t * tree, play_t * play, int value, int depth);
+void fill_play_buffer(play_t * play);
 void cell_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
 void cell_does_not_belongs_to_player(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
 void traversal_rec(board_t * board, tree_t * tree, play_t * play, cell_t * cell, bool * visited, bool player);
