7b717cd719838985efb4b1e9cca47686850e7be3
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-23 17:45:54 +0200

    Initialised akropolis files

---
+++ akropolis/README.md
@@ -0,0 +1,23 @@
+# akropolis
+
+This second project uses the game **Akropolis** to implement **reinforced learning** algorithms such as **MCTS**.
+
+## Roadmap
+- [ ] Create the structure of the game.
+- [ ] Create a graphical interface in SDL2.
+- [ ] Implement the MCTS algorithm.
+
+## Principle
+It is a 1v1 game, it is played against our algorithm.
+
+### Rules
+[...]
+
+### Goal
+
+## To launch the game
+```bash
+make dir
+make
+./bin/main
+```
---
+++ akropolis/src/main.c
@@ -0,0 +1,26 @@
+/* name : main.c
+ * author : matheo thomas
+ * date : 23-06-24
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main(int argc, char *argv[]){
+	if(argc == 2) {
+		if(strcmp(argv[1], "domitille") == 0) {
+			printf("domitille\n");
+		} else if(strcmp(argv[1], "eloi") == 0) {
+			printf("eloi\n");
+		} else if(strcmp(argv[1], "matheo") == 0) {
+			printf("matheo\n");
+		}
+	} else {
+
+	}
+
+	return 0;
+}
+
