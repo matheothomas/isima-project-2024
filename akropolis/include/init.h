d69a53ab3fd576c4943cb55c6f633d1fd07c57fb
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-24 10:17:15 +0200

    Began structures initialisation

--- akropolis/include/init.h
+++ akropolis/include/init.h
@@ -7,11 +7,38 @@
 #define init_h
 
 /* Struct definitions */
+typedef enum cell_type {
+	HOUSE_BLUE,
+	MARKET_YELLOW,
+	BARRAK_RED,
+	TEMPLE_PURPLE,
+	PARK_GREEN,
+	QUARRY_GRAY,
+} cell_type_e;
 
+
+typedef struct cell {
+	int id;
+	cell_type_e cell_type;
+	struct cell *neighbour[6];
+	struct tile *parent;
+
+} cell_t;
+
+typedef struct tile {
+	int id;
+	cell_t *cell_tab[3];
+	int orientation;
+
+} tile_t;
+
+typedef struct board {
+
+
+} board_t;
 
 
 /* Functions definitions */
 
 
 #endif
-
