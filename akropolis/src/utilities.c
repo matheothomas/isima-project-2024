cda0e5e03b00121db16135def52fd04d22a40683
Elloim <eloi.petit.pro@protonmail.com>
2024-06-24 17:23:46 +0200

    Added first implementation of gen_tiles

--- akropolis/src/utilities.c
+++ akropolis/src/utilities.c
@@ -6,11 +6,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <init.h>
 #include <algos.h>
 
 bool validity_tile(tile_t * tile_to_add) {
-	
+
 	// Check if the tile is placed at the right level
 	if (tile_to_add -> cell_tab[0] -> altitude != tile_to_add -> cell_tab[1] -> altitude ||
 		tile_to_add -> cell_tab[0] -> altitude != tile_to_add -> cell_tab[2] -> altitude) {
@@ -82,7 +83,7 @@ void undo_tile(tile_t * tile) {
 }
 
 play_t * gen_tiles(cell_t ** cell_tab, tile_t * tile) {
-	
+
 	play_t * list = malloc(sizeof(play_t));
 	list -> tile = NULL;
 	list -> next = NULL;
@@ -90,22 +91,31 @@ play_t * gen_tiles(cell_t ** cell_tab, tile_t * tile) {
 	play_t * previous = list;
 
 	for (int i = 0; i < CELL_NUMBER; i++) {
-		for (int orientation = 0; orientation < 6; orientation++) {
-			for (int center = 0; center < 3; center++) {
-				play_t * play = malloc(sizeof(play_t));
-				tile_t * new_tile = malloc(sizeof(tile_t));
-				new_tile -> id = tile -> id;
-				new_tile -> orientation = orientation;
+		for (int orientation = 0; orientation < 5; orientation++) {
+			play_t * play = malloc(sizeof(play_t));
+			play -> tile = NULL;
+			play -> next = NULL;
+			tile_t * new_tile = malloc(sizeof(tile_t));
+			new_tile -> id = tile -> id;
+			new_tile -> orientation = orientation;
 
-				if (validity_tile(new_tile)) {
-					previous -> next = play;
-				}
-				else {
-					free(new_tile);
-					free(play);
-				}
+			new_tile -> cell_tab[0] = cell_tab[i];
+			new_tile -> cell_tab[1] = cell_tab[i] -> neighbour[orientation];
+			new_tile -> cell_tab[2] = cell_tab[i] -> neighbour[(orientation + 1) % 6];
+
+			new_tile -> cell_types[0] = tile -> cell_types[0];
+			new_tile -> cell_types[1] = tile -> cell_types[1];
+			new_tile -> cell_types[2] = tile -> cell_types[2];
+
+			if (validity_tile(new_tile)) {
+				play -> tile = new_tile;
+				previous -> next = play;
+			}
+			else {
+				free(new_tile);
+				free(play);
 			}
 		}
 	}
-	return NULL;
+	return list;
 }
