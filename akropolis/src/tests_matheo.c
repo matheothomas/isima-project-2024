1b59058aec076e156754ef5b9f204fc9c94d031e
matheothomas <103116889+matheothomas@users.noreply.github.com>
2024-06-25 18:43:55 +0200

    Added random placement of cells

--- akropolis/src/tests_matheo.c
+++ akropolis/src/tests_matheo.c
@@ -6,8 +6,6 @@
 
 #define DEBUG
 
-
-
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_image.h>
@@ -34,45 +32,48 @@ void test_matheo(){
 
     graphics_t *g = init_sdl();
     
-    // create/init game
-
-    board_t *player=create_board();
-    tile_t *first_player=create_tile();
+    board_t *player = create_board();
+    tile_t *first_player = create_tile();
     fill_tile(first_player, BLUE_PLACE,  QUARRY_GRAY, QUARRY_GRAY);
-    first_player->cell_tab[0]=player->cell;
-    first_player->cell_tab[1]=player->cell->neighbour[0];
-    first_player->cell_tab[2]=player->cell->neighbour[1];
+    first_player->cell_tab[0] = player->cell;
+    first_player->cell_tab[1] = player->cell->neighbour[0];
+    first_player->cell_tab[2] = player->cell->neighbour[1];
     add_tile(first_player);
  
     board_t *bot=create_board();
     tile_t *first_bot=create_tile();
     fill_tile(first_bot, BLUE_PLACE,  QUARRY_GRAY, QUARRY_GRAY);
-    first_bot->cell_tab[0]=bot->cell;
-    first_bot->cell_tab[1]=bot->cell->neighbour[0];
-    first_bot->cell_tab[2]=bot->cell->neighbour[1];
+    first_bot->cell_tab[0] = bot->cell;
+    first_bot->cell_tab[1] = bot->cell->neighbour[0];
+    first_bot->cell_tab[2] = bot->cell->neighbour[1];
 
-    deck_t *deck=create_deck();
+    deck_t *deck = create_deck();
     init_deck(deck);
 
-    game_t *game=create_game();
+    game_t *game = create_game();
     update_game(game, player, bot, deck->deck[0], deck->deck[1], deck);
 
 	tile_t *tile = game->deck->deck[3];
 	tile->cell_tab[0] = game->player->cell->neighbour[0]->neighbour[0];
 	tile->cell_tab[1] = game->player->cell->neighbour[0]->neighbour[0]->neighbour[0];
 	tile->cell_tab[2] = game->player->cell->neighbour[0]->neighbour[0]->neighbour[1];
-	add_tile_to_board(game->player, tile);
-	remove_tile_from_board(game->player, tile);
+	// add_tile_to_board(game->player, tile);
+	// display_board(g, game);
+	// SDL_Delay(1000);
+	// remove_tile_from_board(game->player, tile);
 
+	int n = 0;
 
-    // initialisation of variables
+	// initialisation of variables
 
 	// mouse's position and state
 	int x = 0;
 	int y = 0;
 	int mouse_state = 0;
+	(void)x;
+	(void)y;
 
-    // Initialisation for the event loop
+	// Initialisation for the event loop
 	SDL_bool program_on = SDL_TRUE;
 	SDL_Event event;
 
@@ -89,19 +90,19 @@ void test_matheo(){
 				break;
 
 				case SDL_MOUSEMOTION:
-					x=event.button.x;
-					y=event.button.y;
+					x = event.button.x;
+					y = event.button.y;
 				break;
 
 				case SDL_MOUSEBUTTONDOWN:
-					x=event.button.x;
-					y=event.button.y;
+					x = event.button.x;
+					y = event.button.y;
 					mouse_state=1;
 				break;
 
 				case SDL_MOUSEBUTTONUP:
-					x=event.button.x;
-					y=event.button.y;
+					x = event.button.x;
+					y = event.button.y;
 					mouse_state=2;
 				break;
 
@@ -111,19 +112,26 @@ void test_matheo(){
 		}
 
 		// update
-		if(mouse_state==1){
+		if(mouse_state == 1){
 		}
 
-		if(mouse_state==2){
+		if(mouse_state == 2){
 		}
 
-		// render
-        //display_game(g, game);
-        display_board(g, game);
-		SDL_Delay(1);
+		tile = game->deck->deck[3 + rand() % (DECK_NUMBER - 3)];
+		n = rand() % (CELL_NUMBER) - 40; 
+		tile->cell_tab[0] = game->player->cell_tab[n];
+		tile->cell_tab[1] = game->player->cell_tab[n]->neighbour[0];
+		tile->cell_tab[2] = game->player->cell_tab[n]->neighbour[1];
+		add_tile_to_board(game->player, tile);
+		display_board(g, game);
+		SDL_Delay(300);
+		remove_tile_from_board(game->player, tile);
+		display_board(g, game);
+		SDL_Delay(10);
 	}
 
-    ///*
+	///*
 	SDL_RenderClear(g->renderer);
 
 	IMG_Quit();
@@ -131,9 +139,9 @@ void test_matheo(){
 	end_sdl(1, "Normal ending", g->window, g->renderer);
 	//*/
 
-    for(int i=0;i<34;i++){
-        free(deck->deck[i]);
-    }
+	for(int i = 0; i < 34; i++){
+		free(deck->deck[i]);
+	}
 
-    free(deck);
+	free(deck);
 }
