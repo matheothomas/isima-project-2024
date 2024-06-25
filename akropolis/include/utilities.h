e2a062629f37bbe877b8183456060f831f1b6366
Elloim <eloi.petit.pro@protonmail.com>
2024-06-25 15:57:18 +0200

    Changed files to include hash_map

--- akropolis/include/utilities.h
+++ akropolis/include/utilities.h
@@ -34,12 +34,7 @@ typedef struct linked_plays {
 	play_t * play;
 } linked_plays_t;
 
-/// Linked list for the hash_map. It contains the unmoduled hash of the board and the list of possible plays associated to this board.
-typedef struct hash {
-	uint32_t hashed_board;
-	linked_plays_t * plays;
-	struct hash * next;
-} hash_t;
+
 
 
 /* Functions definitions */
@@ -104,36 +99,6 @@ linked_plays_t * gen_tiles_from_game(game_t * game, bool is_bot);
 /*
  * Hashmaps functions
  */
-
-/// Exponentiation for uint32_t
-uint32_t pow_u32(uint32_t x, int n);
-
-/// Generate an empty hash_map
-hash_t ** create_hash_map();
-
-/// Return the unmoduled hash of a board
-uint32_t hash_board(board_t * board_t);
-
-/// Initialize and fills the hash_t structure
-hash_t * create_linked_hash(uint32_t hashed_board, linked_plays_t * plays, hash_t * next);
-
-/// Merge two plays scores in place
-void merge_plays(play_t * play, play_t * new_play);
-
-/// Adds a board to the hash_map
-void hash_map_add(hash_t ** hash_map, board_t * board, linked_plays_t * plays);
-
-/// Free play_t list
-void free_plays(play_t * plays);
-
-/// Free linked_play_t list
-void free_linked_plays(linked_plays_t * linked_plays);
-
-/// Free all the hash in a hash_map entry
-void free_hash_list(hash_t * hash);
-
-/// Free the hash_map
-void free_hash_map(hash_t ** hash_map);
 
 #endif
 
